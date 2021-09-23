cb0e122defa2e0ddc7654e9c17bee2aaaa4df6f8
EvanDbg <yangf1023@icloud.com>
2021-09-23 13:58:27 +0800

    Update debug-github-actions.sh

--- debug-github-actions.sh
+++ debug-github-actions.sh
@@ -33,7 +33,7 @@ HAS_ERRORS=$(grep "command failed" < .ngrok.log)
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//http:/")"
   echo "=========================================="
 else
   echo "$HAS_ERRORS"
